eb9c3bbc9b95eca8b685d16511e3114fa5336fd1
Mitchell Youngerman <MYounge@am.trimblecorp.net>
2020-06-10 13:49:15 -0400

    Added simple localhost server with routing.

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const fs = require("fs");
 const http = require("http");
 
-const tempOverview = fs.readFileSync(
-  "./templates/template-overview.html",
-  "utf-8"
-);
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
+const tempHome = fs.readFileSync("./templates/index.html", "utf-8");
 const tempProductList = fs.readFileSync(
-  "./templates/template-product-list.html",
+  "./templates/product-list.html",
   "utf-8"
 );
 const tempProductDetails = fs.readFileSync(
-  "./templates/template-product-details.html",
+  "./templates/product-details.html",
   "utf-8"
 );
 
 ////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end(tempOverview);
+  let pathName = req.url;
+
+  if (pathName === "/" || pathName === "/home") {
+    res.end(tempHome);
+  } else if (pathName === "/product-list") {
+    res.end(tempProductList);
+  } else if (pathName === "/product-details") {
+    res.end(tempProductDetails);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found.</h1>");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
   console.log("Listening on port 5000.");
 });
-
-////////////////
-// ROUTING
